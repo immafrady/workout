86256bbc81ec793cda9ded37a21716405073d2e9
Frady <immafrady@gmail.com>
2018-04-04 16:49:53 +0800

    012 Follow Mouse

---
+++ pages/src/012/index.js
@@ -0,0 +1,26 @@
+const divFilter = document.getElementsByClassName("filter")[0];
+
+/**
+ * 追钟鼠标位置
+ * @param ev 事件
+ * @returns {*}
+ */
+mousePosition = (ev) => {
+    if (ev.pageX || ev.pageY) {
+        return {
+            x : ev.pageX,
+            y : ev.pageY
+        }
+    } else {
+        return {
+            x : ev.clientX + document.body.scrollLeft - document.body.clientLeft,
+            y : ev.clientY + document.body.scrollTop - document.body.clientTop
+        }
+    }
+};
+
+document.body.addEventListener("mousemove",ev => {
+    const filter = divFilter.style;
+    const now = mousePosition(ev);
+    filter.cssText = `background: radial-gradient(circle at ${now.x}px ${now.y}px , rgba(0,0,0,0) 0%, rgba(0,0,0,0.98) 20%)`
+},true);
